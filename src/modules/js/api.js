8f450b8c260ddb5b87faa8a66cb6ec7340bffeb2
zhaobw <1964471757@qq.com>
2019-06-28 19:09:54 +0800

    完成分类

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,6 +1,9 @@
 let url ={
     hostLists:'/index/hotLists',
-    banner:'/index/banner'
+    banner:'/index/banner',
+    topList:'/category/topList',
+    subList:'/category/subList',
+    rank:'/category/rank'
 }
 
 let host = 'https://www.easy-mock.com/mock/5d085a6c7295523d0cec9cb9'
