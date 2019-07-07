e4bd704b835d5589499dd58e78cbff555539c69c
zhaobw <1964471757@qq.com>
2019-07-07 10:31:04 +0800

    完成购物车的勾选功能

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -10,10 +10,50 @@ import url from 'js/api.js'
 new Vue({
     el:'.container',
     data:{
-        lists:null
+        lists: null,
+        total: 0,
+        editingShop: null,
+        editingShopIndex: -1
     },
     computed:{
+        allSelected:{
+            get(){
+                if(this.lists && this.lists.length){
+                    return this.lists.every(shop => {
+                        return shop.checked;
+                    })
+                }
+                return false;
+            },
+            set(newval){
+                this.lists.forEach(shop=>{
+                    shop.checked = newval;
+                    shop.goodsList.forEach(good=>{
+                        good.checked = newval;
+                    })
+                })
+            }
+        },
+        selectLists(){
+            if(this.lists && this.lists.length){
+                let arr = [];
+                let total = 0;
+                this.lists.forEach(shop=>{
+                    shop.goodsList.forEach(good=>{
+                        if(good.checked){
+                            arr.push(good);
+                            total += good.price * good.number;
+                        }
+                    })
+                })
+                this.total = total;
+                return arr;
+            }
+            return [];
+        },
+        removeList(){
 
+        }
     },
     created(){
         this.getList();
@@ -23,16 +63,45 @@ new Vue({
             axios.post(url.cartLists).then(res=>{
                 let lists = res.data.cartList;
                 lists.forEach(shop=>{
+                    shop.checked = true;
+                    shop.removeChecked = false;
+                    shop.editing = false;
+                    shop.editingMsg = '编辑';
                     shop.goodsList.forEach(good=>{
                         good.checked = true;
+                        good.removeChecked = false
                     })
                 })
                 this.lists = lists;
             })
         },
-        selectGood(good){
+        selectGood(shop,good){
             good.checked = !good.checked;
-            console.log(this.lists);
+            shop.checked = shop.goodsList.every(good =>{
+                return good.checked;
+            })
+        },
+        selectShop(shop){
+            shop.checked = !shop.checked;
+            shop.goodsList.forEach(good=>{
+                console.log(good);
+                good.checked = shop.checked;
+            })
+        },
+        selectAll(){
+            this.allSelected = !this.allSelected;
+        },
+        edit(shop,shopIndex){
+            shop.editing = !shop.editing;
+            shop.editingMsg =  shop.editing ? '完成' : '编辑'
+            this.lists.forEach((item,i)=>{
+                if(shopIndex !== i){
+                    item.editing = false;
+                    item.editingMsg = shop.editing ? '' : '编辑'
+                }
+            })
+            this.editingShop = shop.editing ? shop : null;
+            this.editingShopIndex = shop.editing ? shopIndex : -1;
         }
     },
     mixins:[mixin]
